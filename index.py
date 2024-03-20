cb5daaed5cc2b49a6cc9c42c64d372582462c87a
igorw <igorwmoura@gmail.com>
2024-03-19 21:22:50 -0300

    indexx

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ def conexaoBanco():
     conexao = mysql.connector.connect(
         host ='localhost',
         user ='root', 
-        password ='IgorJuli@2024', 
+        password ='-------', 
         database ='bdfilmes', 
     )
     cursor = conexao.cursor()
